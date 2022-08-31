c0582e6c6569af24c745bcf8b6cd494d9f0e1903
Azlan <98301786+azlanajju@users.noreply.github.com>
2022-08-31 16:50:43 +0530

    Update main.js

--- main.js
+++ main.js
@@ -26,8 +26,72 @@ function offf() {
   document.getElementById("overlay").style.display = "none";
 }
 
-function myFunction() {
+function myFunction8() {
   var copyText = document.getElementById("myInput");
+  copyText.select();
+  document.execCommand("text-btn")
+ 
+  
+}
+
+function myFunction7() {
+  var copyText = document.getElementById("myInput1");
+  copyText.select();
+  document.execCommand("text-btn")
+ 
+  
+}
+
+function myFunction6() {
+  var copyText = document.getElementById("myInput2");
+  copyText.select();
+  document.execCommand("text-btn")
+ 
+  
+}
+
+function myFunction5() {
+  var copyText = document.getElementById("myInput3");
+  copyText.select();
+  document.execCommand("text-btn")
+ 
+  
+}
+
+function myFunction4() {
+  var copyText = document.getElementById("myInput4");
+  copyText.select();
+  document.execCommand("text-btn")
+ 
+  
+}
+
+function myFunction3() {
+  var copyText = document.getElementById("myInput5");
+  copyText.select();
+  document.execCommand("text-btn")
+ 
+  
+}
+
+function myFunction2() {
+  var copyText = document.getElementById("myInput6");
+  copyText.select();
+  document.execCommand("text-btn")
+ 
+  
+}
+
+function myFunction1() {
+  var copyText = document.getElementById("myInput7");
+  copyText.select();
+  document.execCommand("text-btn")
+ 
+  
+}
+
+function myFunction0() {
+  var copyText = document.getElementById("myInput9");
   copyText.select();
   document.execCommand("text-btn")
  
