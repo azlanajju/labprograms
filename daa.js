cf34392a45268ca3664ffac9be09a5d696b347cc
Azlan <98301786+azlanajju@users.noreply.github.com>
2022-08-31 17:25:16 +0530

    Update daa.js

--- daa.js
+++ daa.js
@@ -1,4 +1,4 @@
-alert('PAGE UNDER MAINTENANCE...');
+
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
 }
@@ -27,10 +27,111 @@ function offf() {
 }
 
 
-function myFunction() {
-  var copyText = document.getElementById("myInput");
+function myFunction00() {
+  var copyText = document.getElementById("myInput00");
   copyText.select();
-  document.execCommand("copyText")
+  document.execCommand("text-btn");
  
   
 }
+function myFunction0() {
+  var copyText = document.getElementById("myInput0");
+  copyText.select();
+  document.execCommand("text-btn");
+ 
+  
+}
+
+
+function myFunction1() {
+  var copyText = document.getElementById("myInput1");
+  copyText.select();
+  document.execCommand("text-btn");
+ 
+  
+}
+
+function myFunction2() {
+  var copyText = document.getElementById("myInput2");
+  copyText.select();
+  document.execCommand("text-btn");
+ 
+  
+}
+
+function myFunction3() {
+  var copyText = document.getElementById("myInput3");
+  copyText.select();
+  document.execCommand("text-btn");
+ 
+  
+}
+
+function myFunction4() {
+  var copyText = document.getElementById("myInput4");
+  copyText.select();
+  document.execCommand("text-btn");
+ 
+  
+}
+
+function myFunction5() {
+  var copyText = document.getElementById("myInput5");
+  copyText.select();
+  document.execCommand("text-btn");
+ 
+  
+}
+
+function myFunction6() {
+  var copyText = document.getElementById("myInput6");
+  copyText.select();
+  document.execCommand("text-btn");
+ 
+  
+}
+
+function myFunction7() {
+  var copyText = document.getElementById("myInput7");
+  copyText.select();
+  document.execCommand("text-btn");
+ 
+  
+}
+
+
+function myFunction8() {
+  var copyText = document.getElementById("myInput8");
+  copyText.select();
+  document.execCommand("text-btn");
+ 
+  
+}
+
+
+function myFunction9() {
+  var copyText = document.getElementById("myInput9");
+  copyText.select();
+  document.execCommand("text-btn");
+ 
+  
+}
+
+
+function myFunction10() {
+  var copyText = document.getElementById("myInput10");
+  copyText.select();
+  document.execCommand("text-btn");
+ 
+  
+}
+
+function myFunction11() {
+  var copyText = document.getElementById("myInput11");
+  copyText.select();
+  document.execCommand("text-btn");
+ 
+  
+}
+
+
