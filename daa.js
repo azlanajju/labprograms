900221c90e8caad007acc6060d9a5c09adbf49a2
Azlan <98301786+azlanajju@users.noreply.github.com>
2022-08-31 16:19:13 +0530

    Update daa.js

--- daa.js
+++ daa.js
@@ -26,3 +26,11 @@ function offf() {
   document.getElementById("overlay").style.display = "none";
 }
 
+
+function myFunction() {
+  var copyText = document.getElementById("myInput");
+  copyText.select();
+  document.execCommand("copyText")
+ 
+  
+}
