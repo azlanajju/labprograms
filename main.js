2eff68eaaf2f0b189f01364594a1028a8cdcf29a
Azlan <98301786+azlanajju@users.noreply.github.com>
2022-09-02 17:33:13 +0530

    Update main.js

--- main.js
+++ main.js
@@ -27,73 +27,101 @@ function offf() {
 }
 
 function myFunction8() {
+  
+ document.getElementById("demo").innerHTML = "Copied!";
+
   var copyText = document.getElementById("myInput");
-  copyText.select();
-  document.execCommand("text-btn")
- 
+  copyText.select(); 
+  navigator.clipboard.writeText(copyText.value);
+
   
 }
 
 function myFunction7() {
+  
+ document.getElementById("demo1").innerHTML = "Copied!";
   var copyText = document.getElementById("myInput1");
-  copyText.select();
-  document.execCommand("text-btn")
+ copyText.select(); 
+ 
+  navigator.clipboard.writeText(copyText.value); 
  
   
 }
 
 function myFunction6() {
+  
+ document.getElementById("demo2").innerHTML = "Copied!";
   var copyText = document.getElementById("myInput2");
   copyText.select();
-  document.execCommand("text-btn")
+  
+  navigator.clipboard.writeText(copyText.value);
+  
  
   
 }
 
 function myFunction5() {
+  
+ document.getElementById("demo3").innerHTML = "Copied!";
   var copyText = document.getElementById("myInput3");
   copyText.select();
-  document.execCommand("text-btn")
+  
+  navigator.clipboard.writeText(copyText.value);
  
   
 }
 
 function myFunction4() {
+  
+ document.getElementById("demo4").innerHTML = "Copied!";
   var copyText = document.getElementById("myInput4");
   copyText.select();
-  document.execCommand("text-btn")
+  
+  navigator.clipboard.writeText(copyText.value);
  
   
 }
 
 function myFunction3() {
+  
+ document.getElementById("demo5").innerHTML = "Copied!";
   var copyText = document.getElementById("myInput5");
   copyText.select();
-  document.execCommand("text-btn")
+  
+  navigator.clipboard.writeText(copyText.value);
  
   
 }
 
 function myFunction2() {
+  
+ document.getElementById("demo6").innerHTML = "Copied!";
   var copyText = document.getElementById("myInput6");
   copyText.select();
-  document.execCommand("text-btn")
+  
+  navigator.clipboard.writeText(copyText.value);
  
   
 }
 
 function myFunction1() {
+  
+ document.getElementById("demo7").innerHTML = "Copied!";
   var copyText = document.getElementById("myInput7");
   copyText.select();
-  document.execCommand("text-btn")
+  
+  navigator.clipboard.writeText(copyText.value);
  
   
 }
 
 function myFunction0() {
+  
+ document.getElementById("demo8").innerHTML = "Copied!";
   var copyText = document.getElementById("myInput9");
   copyText.select();
-  document.execCommand("text-btn")
+  
+  navigator.clipboard.writeText(copyText.value);
  
   
 }
