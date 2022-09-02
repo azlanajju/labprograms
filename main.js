f5c39dcc87dfe48c6e5e37e4ce71d5dd9fb4225c
Azlan <98301786+azlanajju@users.noreply.github.com>
2022-09-02 17:47:44 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
   jSuites.notification({
         name: 'WELCOME',
-        message: 'by azlan',
+        
     }); 
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
