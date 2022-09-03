60544a175f3c5dc57437010e3196f1bacbdfda0c
Azlan <98301786+azlanajju@users.noreply.github.com>
2022-09-03 16:55:42 +0530

    Update daa.js

--- daa.js
+++ daa.js
@@ -26,48 +26,117 @@ function offf() {
   document.getElementById("overlay").style.display = "none";
 }
 
+/* ----------------button--------------*/
 
+/*
+function myFunction8() {
+  
+  jSuites.notification({
+        name: 'copied',
+        message:'program1'
+        
+    }); 
+ document.getElementById("demo").innerHTML = "Copied!";
+
+  var copyText = document.getElementById("myInput");
+  copyText.select(); 
+  navigator.clipboard.writeText(copyText.value);
+
+  
+} */
 function myFunction00() {
+ 
+  jSuites.notification({
+        name: 'copied',
+        message:'program1'
+         
+    }); 
+    
+ document.getElementById("demo00").innerHTML = "Copied!";
+
   var copyText = document.getElementById("myInput00");
   copyText.select();
-  document.execCommand("text-btn");
+  
+  navigator.clipboard.writeText(copyText.value);
  
   
 }
 function myFunction0() {
+  
+  jSuites.notification({
+        name: 'copied',
+        message:'program1b'
+         
+    }); 
+    
+ document.getElementById("demo0").innerHTML = "Copied!";
   var copyText = document.getElementById("myInput0");
   copyText.select();
-  document.execCommand("text-btn");
+  
+  navigator.clipboard.writeText(copyText.value);
  
   
 }
 
 
 function myFunction1() {
+  
+  jSuites.notification({
+        name: 'copied',
+        message:'program2a'
+         
+    }); 
+    
+ document.getElementById("demo1").innerHTML = "Copied!";
   var copyText = document.getElementById("myInput1");
   copyText.select();
-  document.execCommand("text-btn");
- 
+  
+ navigator.clipboard.writeText(copyText.value);
   
 }
 
 function myFunction2() {
+  
+  jSuites.notification({
+        name: 'copied',
+        message:'program2b'
+         
+    }); 
+    
+ document.getElementById("demo2").innerHTML = "Copied!";
   var copyText = document.getElementById("myInput2");
   copyText.select();
-  document.execCommand("text-btn");
+  navigator.clipboard.writeText(copyText.value);
  
   
 }
 
 function myFunction3() {
+  
+  jSuites.notification({
+        name: 'copied',
+        message:'program3a'
+         
+    }); 
+    
+ document.getElementById("demo3").innerHTML = "Copied!";
   var copyText = document.getElementById("myInput3");
   copyText.select();
-  document.execCommand("text-btn");
+  
+  navigator.clipboard.writeText(copyText.value);
  
   
 }
 
 function myFunction4() {
+  
+  jSuites.notification({
+        name: 'copied',
+        message:'program3b'
+         
+    }); 
+    
+ document.getElementById("demo4").innerHTML = "Copied!";
   var copyText = document.getElementById("myInput4");
   copyText.select();
   document.execCommand("text-btn");
@@ -76,60 +145,123 @@ function myFunction4() {
 }
 
 function myFunction5() {
+  
+  jSuites.notification({
+        name: 'copied',
+        message:'program4'
+         
+    }); 
+    
+ document.getElementById("demo5").innerHTML = "Copied!";
   var copyText = document.getElementById("myInput5");
   copyText.select();
-  document.execCommand("text-btn");
+  
+  navigator.clipboard.writeText(copyText.value);
  
   
 }
 
 function myFunction6() {
+  
+  jSuites.notification({
+        name: 'copied',
+        message:'program5'
+         
+    }); 
+    
+ document.getElementById("demo6").innerHTML = "Copied!";
   var copyText = document.getElementById("myInput6");
   copyText.select();
-  document.execCommand("text-btn");
+ 
+  navigator.clipboard.writeText(copyText.value);
  
   
 }
 
 function myFunction7() {
+  
+  jSuites.notification({
+        name: 'copied',
+        message:'program6a'
+         
+    }); 
+    
+ document.getElementById("demo7").innerHTML = "Copied!";
   var copyText = document.getElementById("myInput7");
   copyText.select();
-  document.execCommand("text-btn");
+  
+  navigator.clipboard.writeText(copyText.value);
  
   
 }
 
 
 function myFunction8() {
+  
+  jSuites.notification({
+        name: 'copied',
+        message:'program6b'
+         
+    }); 
+    
+ document.getElementById("demo8").innerHTML = "Copied!";
   var copyText = document.getElementById("myInput8");
   copyText.select();
-  document.execCommand("text-btn");
+ 
+  navigator.clipboard.writeText(copyText.value);
  
   
 }
 
 
 function myFunction9() {
+  
+  jSuites.notification({
+        name: 'copied',
+        message:'program7'
+         
+    }); 
+    
+ document.getElementById("demo9").innerHTML = "Copied!";
   var copyText = document.getElementById("myInput9");
   copyText.select();
-  document.execCommand("text-btn");
+  
+  navigator.clipboard.writeText(copyText.value);
  
   
 }
 
 
 function myFunction10() {
+  
+  jSuites.notification({
+        name: 'copied',
+        message:'program8'
+         
+    }); 
+    
+ document.getElementById("demo10").innerHTML = "Copied!";
   var copyText = document.getElementById("myInput10");
   copyText.select();
-  document.execCommand("text-btn");
+ 
+  navigator.clipboard.writeText(copyText.value);
  
   
 }
 
 function myFunction11() {
+  
+  jSuites.notification({
+        name: 'copied',
+        message:'program9'
+         
+    }); 
+    
+ document.getElementById("demo11").innerHTML = "Copied!";
   var copyText = document.getElementById("myInput11");
   copyText.select();
-  document.execCommand("text-btn");
+  
+  navigator.clipboard.writeText(copyText.value);
  
   
 }
